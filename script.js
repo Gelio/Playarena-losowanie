e4c922f153de97ab53f8f3ea1d02911fdade3e44
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2015-04-28 17:15:08 +0200

    almost finished, need to random all the teams before showing

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var druzyny;
+var step = 1;
+var losowanie = false;
+
 function blad(tresc)
 {
     $("div#errorMessage").show().text(tresc);
@@ -18,21 +22,82 @@ function initial()
 
 function losuj()
 {
-    var druzyny = $("textarea#inputs").val().split("\n");
+    if(losowanie)
+    {
+        return losujStep();
+    }
+
+    druzyny = $("textarea#inputs").val().split("\n");
     if(druzyny.length < 16) {
         blad("Zbyt mała liczba drużyn, należy wpisać 16.");
         return true;
     }
 
-    
     resetujBlad();
+
+    for(var i=0; i < 16; i++)
+    {
+        druzyny[i] = {
+            'name': druzyny[i],
+            'picked': false
+        };
+    }
+
+    step = 1;
+    losowanie = true;
+    $("#chooseSingle").prop("disabled", true);
+    if(!$("#chooseSingle").is(":checked"))
+        $("#losuj").prop("disabled", true);
+    losujStep();
+
+    return true;
+}
+
+function losujStep()
+{
+    var aktualnaLista = [];
+    //alert("a");
+    if(step%2 == 1) // liderzy
+    {
+        //alert("b");
+        for(var i=0; i < 16; i+=2)
+        {
+            if(!druzyny[i].picked)
+                aktualnaLista.push(i);
+        }
+    }
+    else
+    {
+        //alert("c");
+        for(var i=1; i < 16; i+=2)
+        {
+            if(!druzyny[i].picked && $("#team".concat(step-1)).text() != druzyny[i-1].name)
+                aktualnaLista.push(i);
+        }
+    }
+    //alert("d");
+    var wylosowana = Math.floor(Math.random()*aktualnaLista.length);
+    //alert(wylosowana);
+    $("#team".concat(step)).addClass("druzynaPicked").text(druzyny[aktualnaLista[wylosowana]].name);
+    druzyny[aktualnaLista[wylosowana]].picked = true;
+
+    if(step == 17)
+        $("#losuj").prop("disabled", true);
+
+    step++;
+    if(step <= 16 && !$("#chooseSingle").is(":checked"))
+        return losujStep();
 }
 
 function reset()
 {
-    $("textarea#inputs").val("");
+    //$("textarea#inputs").val("");
     $("span.druzynaPicked").text("?");
     $("span.druzynaPicked").removeClass("druzynaPicked");
+    $("#chooseSingle").prop("disabled", false);
+    $("#losuj").prop("disabled", false);
+    losowanie = false;
+    step = 1;
     resetujBlad();
 }
 
