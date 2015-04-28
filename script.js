b667fa0f293d1b9e0f48aba389ed2ef74b0409af
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2015-04-28 16:41:20 +0200

    prepared for selection implementation

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
+function blad(tresc)
+{
+    $("div#errorMessage").show().text(tresc);
+    return true;
+}
+
+function resetujBlad()
+{
+    $("div#errorMessage").hide();
+    return true;
+}
+
 function initial()
 {
-    $("#losuj").click(losuj);
-    $("#reset").click(reset);
+    $("button#losuj").click(losuj);
+    $("button#reset").click(reset);
 }
 
 function losuj()
 {
+    var druzyny = $("textarea#inputs").val().split("\n");
+    if(druzyny.length < 16) {
+        blad("Zbyt mała liczba drużyn, należy wpisać 16.");
+        return true;
+    }
 
+    
+    resetujBlad();
 }
 
 function reset()
 {
-
+    $("textarea#inputs").val("");
+    $("span.druzynaPicked").text("?");
+    $("span.druzynaPicked").removeClass("druzynaPicked");
+    resetujBlad();
 }
 
 window.onload = initial;
